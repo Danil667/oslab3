c2532f5f1afd3e2e37a0d7b5bb2c8c76a72779fe
danil732000@gmail.com <danil732000@gmail.com>
2020-07-02 17:37:08 +0400

    Initial commit

---
+++ src/Page.java
@@ -0,0 +1,21 @@
+//непрерывный диапазон адресов
+public class Page {
+    private String data = "";
+    private int frequency=0;
+
+    public Page(String data) {
+        this.data = data;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public int getFrequency() {
+        return frequency;
+    }
+
+    public void use() {
+        frequency++;
+    }
+}
